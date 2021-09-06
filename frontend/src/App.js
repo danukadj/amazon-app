707e0be12c532e1bd888d0246170cf1e0f34f63d
danukadj <danukasandaruwandj@gmail.com>
2021-09-06 08:06:37 +0530

    component-Product,Rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import Product from './components/Product';
 import data from './data';
 
 function App() {
@@ -5,9 +6,7 @@ function App() {
     <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="/">
-            amazona
-          </a>
+          <a className="brand" href="/">amazon</a>
         </div>
         <div>
           <a href="/cart">Cart</a>
@@ -18,38 +17,7 @@ function App() {
         <div>
           <div className="row center">
             {data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img
-                    className="medium"
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  <div className="price">${product.price}</div>
-                </div>
-              </div>
+              <Product key={product._id} product={product}></Product>
             ))}
           </div>
         </div>
